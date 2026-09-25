107366952348fc76c00603c4a4bc02774a923d8b
ryanbytes <45298254+ryanbytes@users.noreply.github.com>
2026-09-25 05:50:44 -0400

    Fix multi-device entity collisions

--- custom_components/oupes_mega2/config_flow.py
+++ custom_components/oupes_mega2/config_flow.py
@@ -54,7 +54,12 @@ class OupesMega2ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "cannot_connect"
             else:
-                return self.async_create_entry(title="OUPES Mega 2", data=user_input)
+                await self.async_set_unique_id(user_input[CONF_DEVICE_ID])
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=f"OUPES Mega 2 ({user_input[CONF_DEVICE_ID]})",
+                    data=user_input,
+                )
 
         return self.async_show_form(
             step_id="user",
